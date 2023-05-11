f0979acf6cf353fd145d8b55eb9790bfdf0b0c01
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-05-11 16:37:02 -0700

    commandLine

--- frontend/components/Inputbox.jsx
+++ frontend/components/Inputbox.jsx
@@ -2,11 +2,14 @@
 
 export default function InputMessage() {
     return (
-        <input
-            className="absolute bottom-[-200px] left-1/2 transform -translate-x-1/2 w-4/5 p-4 text-2xl rounded-10 border-none"
-              type="text"
-            placeholder="Type here..."
-      />
+        <div className="absolute bottom-[-200px] left-1/2 transform -translate-x-1/2 w-4/5 p-4 rounded-10 flex items-center text-green-400 bg-black">
+            <span className="text-2xl font-mono">$</span>
+            <input
+                className="text-2xl font-mono bg-black pl-2 w-full text-green-400 border-none"
+                type="text"
+                placeholder="Type your command..."
+            />
+        </div>
     )
   }
   
