bc580ac5bbd872bcdc00ae512e4e2d582c7a44ee
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-05-11 17:35:05 -0700

    sidebar theme

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -2,8 +2,8 @@ import FileUpload from './UpLoadFile';
 
 export default function Sidebar() {
     return (
-        <div className="relative w-[300px] h-full bg-gray-800 float-right text-white text-center pt-40">
-            Sidebar
+        <div className="relative w-[300px] h-full bg-black text-green-400 float-right text-center pt-10">
+            <h1 className="text-xl mb-5">Sidebar</h1>
             <div className="mt-5">
                 <FileUpload />
             </div>
--- frontend/components/UpLoadFile.jsx
+++ frontend/components/UpLoadFile.jsx
@@ -1,12 +1,19 @@
 'use client'
-import React, { useState } from 'react';
+
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 const FileUpload = () => {
   const [file, setFile] = useState(null);
 
-  const onFileChange = (e) => {
-    setFile(e.target.files[0]);
-  }
+  const onDrop = useCallback(acceptedFiles => {
+    setFile(acceptedFiles[0]);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ 
+    onDrop, 
+    accept: 'text/csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' 
+  });
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -23,9 +30,17 @@ const FileUpload = () => {
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <input type="file" onChange={onFileChange} />
-      <button type="submit">Upload</button>
+    <form onSubmit={onSubmit} className="flex flex-col bg-black text-green-400 p-6 rounded-lg shadow-lg">
+        <div {...getRootProps()} className="dropzone flex items-center justify-center h-32 border-2 border-dashed border-green-400 rounded cursor-pointer">
+            <input {...getInputProps()} />
+            {isDragActive ? <p>Drop the files here...</p> : <p>Drag 'n' drop some files here, or click to select files</p>}
+        </div>
+        <button 
+            type="submit" 
+            className="mt-4 bg-green-400 text-black px-4 py-2 rounded shadow-md hover:bg-green-500 active:bg-green-600 transition-colors duration-200"
+        >
+            Upload
+        </button>
     </form>
   )
 }
