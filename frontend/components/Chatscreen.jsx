7ce83a4cb7362a075fad8acbe95e7b5815638b00
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-05-15 15:28:17 -0700

    better design

--- frontend/components/Chatscreen.jsx
+++ frontend/components/Chatscreen.jsx
@@ -21,12 +21,32 @@ export default function Screen({ className }) {
 ██╔══╝   ██╔██╗ ██║     ██╔══╝  ██║     ██║   ██║██╔═══╝    ██║   
 ███████╗██╔╝ ██╗╚██████╗███████╗███████╗╚██████╔╝██║        ██║   
 ╚══════╝╚═╝  ╚═╝ ╚═════╝╚══════╝╚══════╝ ╚═════╝ ╚═╝        ╚═╝
-                                                                          
-ExcelGPT [Version 0]
-Jasan Gill. All rights reserved.
-C:\\Users\\ExcelAi: welcome to ExcelAi...`}
+`}
+<h2 className="text-green-400 font-mono ">Data Science Assistant</h2>
+        
+        
+        <h3 className="text-green-400 font-mono mt-4">Example Usage</h3>
+        <ol className="font-mono list-decimal list-inside text-left">
+            <li>Upload a CSV file containing sales data</li>
+            <li>Ask ExcelAI questions</li>
+            <li>Receive responses from ExcelGPT</li>
+        </ol>
+        <p className="font-mono mt-4">
+            ExcelAI simplifies data analysis tasks by providing an intuitive conversational interface and leveraging the power of AI.
+        </p>
+{` 
++------------------------------------------+
+|                                          |
+| ExcelGPT [Version 0]                     | 
+| Jasan Gill. All rights reserved.         |
+| C:\\Users\\ExcelAi: welcome to ExcelAi...  |
+|                                          |
++------------------------------------------+
+`}
               </code>
             </pre>
+            
+        
            </div>
 
         ) : (
