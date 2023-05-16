5bb28f0568f265b3efab9fdb91c065a824dca244
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-05-15 18:55:31 -0700

    98

--- frontend/components/Inputbox.jsx
+++ frontend/components/Inputbox.jsx
@@ -13,7 +13,7 @@ export default function InputMessage() {
     const handleKeyPress = async (e) => {
         if (e.key === 'Enter') {
           
-          const res = await fetch('https://excelsior-7wyb.vercel.app/send_message', {
+          const res = await fetch('http://127.0.0.1:5000/send_message', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message }),
--- frontend/components/UpLoadFile.jsx
+++ frontend/components/UpLoadFile.jsx
@@ -23,7 +23,7 @@ const FileUpload = () => {
     const formData = new FormData();
     formData.append('file', file);
   
-    const res = await fetch('https://excelsior-7wyb.vercel.app/', {
+    const res = await fetch('http://127.0.0.1:5000/', {
       method: 'POST',
       body: formData,
     });
