28d76bc9ce5db7bfd1078193be64a5c2cac5b03c
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-05-16 19:21:04 -0700

    Changed State

--- frontend/components/Chatscreen.jsx
+++ frontend/components/Chatscreen.jsx
@@ -10,10 +10,9 @@ export default function Screen({ className }) {
   return (
     <div className={`${className} relative bg-black overflow-auto p-4 sm:p-6 lg:p-8`}>
       <div className="text-white text-lg sm:text-xl lg:text-2xl">
-        {messages.length === 0 ? (
-           <div>
-            <pre className="font-mono text-white bg-black p-2 rounded text-xs sm:text-sm lg:text-base hidden sm:block">
-              <code>
+        <div>
+          <pre className="font-mono text-white bg-black p-2 rounded text-xs sm:text-sm lg:text-base hidden sm:block">
+            <code>
 {` 
 ███████╗██╗  ██╗ ██████╗███████╗██╗      ██████╗ ██████╗ ████████╗
 ██╔════╝╚██╗██╔╝██╔════╝██╔════╝██║     ██╔════╝ ██╔══██╗╚══██╔══╝
@@ -22,21 +21,20 @@ export default function Screen({ className }) {
 ███████╗██╔╝ ██╗╚██████╗███████╗███████╗╚██████╔╝██║        ██║   
 ╚══════╝╚═╝  ╚═╝ ╚═════╝╚══════╝╚══════╝ ╚═════╝ ╚═╝        ╚═╝
 `}
-</code>
-</pre>
-<h2 className="text-green-400 font-mono text-lg sm:text-xl lg:text-2xl">Data Science Assistant</h2>
-        
-<h3 className="text-green-400 font-mono mt-4 text-md sm:text-lg lg:text-xl">Example Usage</h3>
-<ol className="font-mono list-decimal list-inside text-left text-sm sm:text-md lg:text-lg">
-  <li>Upload an EXCEL file containing data</li>
-  <li>Ask ExcelGPT questions</li>
-  <li>Receive responses from ExcelGPT</li>
-</ol>
-<p className="font-mono mt-4 text-sm sm:text-md lg:text-lg">
-  ExcelAI simplifies data analysis tasks by providing an intuitive conversational interface and leveraging the power of AI.
-</p>
-<pre className="font-mono text-white bg-black p-2 rounded text-xs sm:text-sm lg:text-base  sm:block">
-<code>
+ </code>
+          </pre>
+          <h2 className="text-green-400 font-mono text-lg sm:text-xl lg:text-2xl">Data Science Assistant</h2>
+          <h3 className="text-green-400 font-mono mt-4 text-md sm:text-lg lg:text-xl">Example Usage</h3>
+          <ol className="font-mono list-decimal list-inside text-left text-sm sm:text-md lg:text-lg">
+            <li>Upload an EXCEL file containing data</li>
+            <li>Ask ExcelGPT questions</li>
+            <li>Receive responses from ExcelGPT</li>
+          </ol>
+          <p className="font-mono mt-4 text-sm sm:text-md lg:text-lg">
+            ExcelAI simplifies data analysis tasks by providing an intuitive conversational interface and leveraging the power of AI.
+          </p>
+          <pre className="font-mono text-white bg-black p-2 rounded text-xs sm:text-sm lg:text-base  sm:block">
+            <code>
 {` 
 +------------------------------------------+
 |                                          |
@@ -46,15 +44,14 @@ export default function Screen({ className }) {
 |                                          |
 +------------------------------------------+
 `}
-</code>
-</pre>
-           </div>
-
-        ) : (
+     </code>
+          </pre>
+        </div>
+        {
           messages.map((message, index) => (
             <ChatMessage key={index} message={message.text} isUser={message.isUser} />
           ))
-        )}
+        }
       </div>
     </div>
   );
