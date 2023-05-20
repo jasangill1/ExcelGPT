b7bab94f2c8d65cca622c5204fac6e5d0421b968
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-05-20 12:25:56 -0700

    Update

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -35,7 +35,7 @@ export default function Home() {
       <main className="w-full h-full flex justify-center items-center flex-grow relative bg-black border-gray-800 shadow-2xl">  
         <ChatProvider>
           <Sidebar className={'absolute left-0 top-0 bottom-0 w-full'} />
-          <div className="flex flex-col h-full w-full pl-[300px] sm:pl-0 pt-4 pb-16 sm:pb-4 pr-4 overflow-auto"> 
+          <div className="flex flex-col h-full w-full sm:pl-0 pt-4 pb-16 sm:pb-4 pr-4 overflow-auto"> 
             <Screen />
             <InputMessage className="absolute bottom-0 left-0 right-0 w-full"/>
           </div>
