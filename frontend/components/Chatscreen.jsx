40a0de898f5ad8932358696c3de34332fc1885ef
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-05-24 15:15:07 -0700

    Update Screen

--- frontend/components/Chatscreen.jsx
+++ frontend/components/Chatscreen.jsx
@@ -14,38 +14,31 @@ export default function Screen({ className }) {
 
   return (
     <div
-    className={`${className} relative bg-color5 overflow-auto p-8 sm:p-10 lg:p-12`}
+    className={`${className} relative bg-color5 overflow-auto p-8 sm:p-10 lg:p-12 shadow-lg rounded-lg`}
     ref={containerRef}
-  >
+>
     <div className="text-color1 text-lg sm:text-xl lg:text-2xl">
-      <pre className="font-sans text-color1 bg-color4 p-3 rounded text-sm sm:text-base lg:text-lg">
-        <code>
-            {` 
-███████╗██╗  ██╗ ██████╗███████╗██╗      ██████╗ ██████╗ ████████╗
-██╔════╝╚██╗██╔╝██╔════╝██╔════╝██║     ██╔════╝ ██╔══██╗╚══██╔══╝
-█████╗   ╚███╔╝ ██║     █████╗  ██║     ██║  ███╗██████╔╝   ██║   
-██╔══╝   ██╔██╗ ██║     ██╔══╝  ██║     ██║   ██║██╔═══╝    ██║   
-███████╗██╔╝ ██╗╚██████╗███████╗███████╗╚██████╔╝██║        ██║   
-╚══════╝╚═╝  ╚═╝ ╚═════╝╚══════╝╚══════╝ ╚═════╝ ╚═╝        ╚═╝
-`}
-         </code>
-        </pre>
-        <h2 className="text-color2 font-sans text-lg sm:text-xl lg:text-2xl">Data Science Assistant</h2>
-        <h3 className="text-color2 font-sans mt-4 text-md sm:text-lg lg:text-xl">Example Usage</h3>
-        <ol className="font-sans list-decimal list-inside text-left text-sm sm:text-md lg:text-lg text-color3">
-          <li>Upload an EXCEL file containing data</li>
-          <li>Ask ExcelGPT questions</li>
-          <li>Receive responses from ExcelGPT</li>
-        </ol>
+        
+        <div className="bg-white rounded-lg shadow-lg p-6">
+            <h3 className="text-color2 font-sans mt-4 text-md sm:text-lg lg:text-xl">Example Usage</h3>
+            <ol className="font-sans list-decimal list-inside text-left text-sm sm:text-md lg:text-lg text-color3">
+              <li>Upload an EXCEL file containing data</li>
+              <li>Ask ExcelGPT questions</li>
+              <li>Receive responses from ExcelGPT</li>
+            </ol>
+            <h3 className="text-color2 font-sans mt-4 text-md sm:text-lg lg:text-xl">Limitations</h3>
+            <ul className="font-sans list-disc list-inside text-left text-sm sm:text-md lg:text-lg text-color3">
+              <li>Web version only</li>
+              <li>File size is limited</li>
+            </ul>
+         </div>
+
         <p className="font-sans mt-4 text-sm sm:text-md lg:text-lg text-color3">
-          ExcelAI simplifies data analysis tasks by providing an intuitive conversational interface and leveraging the power of AI.
+            ExcelAI simplifies data analysis tasks by providing an intuitive conversational interface and leveraging the power of AI.
         </p>
         <pre className="font-sans text-color1 bg-color4 p-3 rounded text-sm sm:text-base lg:text-lg">
-
-          <code>
-            {`ExcelGPT [Version 0] Beta                
-C:\\Users\\ExcelAi: welcome to ExcelAi...  
-
+            <code>
+                {`C:\\Users\\ExcelAi: welcome to ExcelAi...  
 `}
           </code>
         </pre>
