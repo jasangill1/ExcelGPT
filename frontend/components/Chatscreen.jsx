4beff372c10030542f2ef37a13d2b8c6d3c8794f
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-05-26 15:57:49 -0700

    update

--- frontend/components/Chatscreen.jsx
+++ frontend/components/Chatscreen.jsx
@@ -26,10 +26,10 @@ export default function Screen({ className }) {
         <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
           <div className="bg-primary rounded-lg shadow-xl p-6 text-quinary">
             <h2 className="font-courier mt-4 text-md sm:text-lg lg:text-xl text-center text-bold pb-2">Usage</h2>
-            <ul className="font-courier list-decimal list-inside text-left text-sm sm:text-md lg:text-lg">
-              <li>Upload an EXCEL file containing data</li>
-              <li>Ask ExcelGPT questions</li>
-              <li>Receive responses from ExcelGPT</li>
+            <ul className="font-courier list-disc list-inside text-left text-sm sm:text-md lg:text-lg">
+              <li style={{ textIndent: '-1em', paddingLeft: '1em' }}>Upload an EXCEL file containing data</li>
+              <li >Ask ExcelGPT questions</li>
+              <li style={{ textIndent: '-1.em', paddingLeft: '1em' }}>Receive responses from ExcelGPT</li>
             </ul>
           </div>
 
@@ -38,16 +38,16 @@ export default function Screen({ className }) {
             <ul className="font-courier list-disc list-inside text-left text-sm sm:text-md lg:text-lg">
               <li>Web version only</li>
               <li>File size is limited</li>
-              <li>Usage timeout after 30 seconds</li>
+              <li style={{ textIndent: '-1.em', paddingLeft: '1em' }}s>Usage timeout if task is taking to long</li>
             </ul>
           </div>
 
           <div className="bg-primary rounded-lg shadow-xl p-6 text-quinary">
             <h2 className="font-courier mt-4 text-md sm:text-lg lg:text-xl text-center text-bold pb-2">Tips</h2>
-            <ul className="font-courier list-decimal list-inside text-left text-sm sm:text-md lg:text-lg">
-              <li>Refresh if stuck on thinking</li>
-              <li>Keep datasheet simple, make it easy to read</li>
-              <li>Give feedback for feature you think are good!</li>
+            <ul className="font-courier list-disc	 list-inside text-left text-sm sm:text-md lg:text-lg">
+              <li style={{ textIndent: '-1.3em', paddingLeft: '1em' }}>Refresh if stuck on thinking</li>
+              <li style={{ textIndent: '-1.3em', paddingLeft: '1em' }}>Keep datasheet simple, make it easy to read</li>
+              <li style={{ textIndent: '-1.3em', paddingLeft: '1em' }}>Give feedback for features you would like added</li>
             </ul>
           </div>
         </div>
