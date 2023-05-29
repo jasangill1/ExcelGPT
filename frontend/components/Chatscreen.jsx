09cf8cd3bd941262a4b0e3be0e9b51e5f0fa59ca
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-05-28 19:23:36 -0700

    adjustments

--- frontend/components/Chatscreen.jsx
+++ frontend/components/Chatscreen.jsx
@@ -19,43 +19,43 @@ export default function Screen({ className }) {
       ref={containerRef}
     >
       <div className="text-center pb-4">
-        <h1 className="inline mr-2 text-quinary text-xl mb-6">Excel GPT</h1>
-        <span className="bg-quinary text-white px-2 py-1 rounded text-sm">Beta</span>
-      </div>
+        
+      <span className="bg-quinary text-white px-2 py-1 rounded text-xl font-bold">Beta</span>
+</div>
 
-      <div className="text-quinary text-lg sm:text-xl lg:text-2xl">
-        <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
-          <div className="bg-primary rounded-lg shadow-xl p-6 text-quinary">
-            
-            <h2 className="font-courier mt-4 text-md sm:text-lg lg:text-xl text-center text-bold pb-2">Usage</h2>
-            <ul className="font-courier list-disc list-inside text-left text-sm sm:text-md lg:text-lg">
-              <li style={{ textIndent: '-1.3em', paddingLeft: '1em'}}>Upload an EXCEL file  containing data</li>
-              <li style={{ textIndent: '-1.3em', paddingLeft: '1em'}}>Ask ExcelGPT questions</li>
-              <li style={{ textIndent: '-1.3em', paddingLeft: '1em'}}>A python agent will be created to write and execute code to find your answer</li>
-              <li style={{ textIndent: '-1.3em', paddingLeft: '1em'}}>Receive answer from ExcelGPT</li>
-            </ul>
-          </div>
+<div className="text-quinary text-base sm:text-lg lg:text-xl">
+  <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
+    <div className="bg-primary rounded-lg shadow-xl p-6 text-quinary ">
+      <h2 className="font-courier mt-4 text-sm sm:text-base lg:text-lg text-center font-bold pb-2">Usage</h2>
+      <ul className="font-courier list-disc list-inside text-left text-xs sm:text-sm lg:text-base">
+        <li style={{ textIndent: '-1.3em', paddingLeft: '1em'}}>Upload an EXCEL file  containing data</li>
+        <li style={{ textIndent: '-1.3em', paddingLeft: '1em'}}>Ask ExcelGPT questions</li>
+        <li style={{ textIndent: '-1.3em', paddingLeft: '1em'}}>A python agent will be created to write and execute code to find your answer</li>
+        <li style={{ textIndent: '-1.3em', paddingLeft: '1em'}}>Receive answer from ExcelGPT</li>
+      </ul>
+    </div>
 
-          <div className="bg-primary rounded-lg shadow-xl p-6 text-quinary">
-            <h2 className="font-courier mt-4 text-md sm:text-lg lg:text-xl text-center text-bold pb-2">Limitations</h2>
-            <ul className="font-courier list-disc list-inside text-left text-sm sm:text-md lg:text-lg">
-              <li style={{ textIndent: '-1.3em', paddingLeft: '1em'  }}>Web version only</li>
-              <li style={{ textIndent: '-1.3em', paddingLeft: '1em'  }}>File size is limited</li>
-              <li style={{ textIndent: '-1.3em', paddingLeft: '1em'  }}>Usage timeout if task is taking to long</li>
-              <li style={{ textIndent: '-1.3em', paddingLeft: '1em'  }}>Caution ExcelGPT is prone to error always make sure to do your own research </li>
-            </ul>
-          </div>
+    <div className="bg-primary rounded-lg shadow-xl p-6 text-quinary">
+      <h2 className="font-courier mt-4 text-sm sm:text-base lg:text-lg text-center font-bold pb-2">Limitations</h2>
+      <ul className="font-courier list-disc list-inside text-left text-xs sm:text-sm lg:text-base">
+        <li style={{ textIndent: '-1.3em', paddingLeft: '1em'  }}>Web version only</li>
+        <li style={{ textIndent: '-1.3em', paddingLeft: '1em'  }}>File size is limited</li>
+        <li style={{ textIndent: '-1.3em', paddingLeft: '1em'  }}>Usage timeout if task is taking to long</li>
+        <li style={{ textIndent: '-1.3em', paddingLeft: '1em'  }}>Caution ExcelGPT is prone to error always make sure to do your own research </li>
+      </ul>
+    </div>
 
-          <div className="bg-primary rounded-lg shadow-xl p-6 text-quinary">
-            <h2 className="font-courier mt-4 text-md sm:text-lg lg:text-xl text-center text-bold pb-2">Tips</h2>
-            <ul className="font-courier list-disc	 list-inside text-left text-sm sm:text-md lg:text-lg">
-              <li style={{ textIndent: '-1.3em', paddingLeft: '1em' }}>Refresh if stuck on thinking</li>
-              <li style={{ textIndent: '-1.3em', paddingLeft: '1em' }}>Keep datasheet simple, make it easy to read</li>
-              <li style={{ textIndent: '-1.3em', paddingLeft: '1em' }}>Give feedback for features you would like added</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+    <div className="bg-primary rounded-lg shadow-xl p-6 text-quinary ">
+      <h2 className="font-courier mt-4 text-sm sm:text-base lg:text-lg text-center font-bold pb-2">Tips</h2>
+      <ul className="font-courier list-disc list-inside text-left text-xs sm:text-sm lg:text-base">
+        <li style={{ textIndent: '-1.3em', paddingLeft: '1em' }}>Refresh if stuck on thinking</li>
+        <li style={{ textIndent: '-1.3em', paddingLeft: '1em' }}>Keep datasheet simple, make it easy to read</li>
+        <li style={{ textIndent: '-1.3em', paddingLeft: '1em' }}>Give feedback for features you would like added</li>
+      </ul>
+    </div>
+  </div>
+</div>
+
 
       <div className="flex-grow overflow-auto">
         {messages.map((message, index) => (
